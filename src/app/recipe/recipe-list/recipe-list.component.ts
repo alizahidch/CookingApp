35cb4ee0a339f829b7be548825f731bd3f038b6e
alizahidch <cmalizahid@gmail.com>
2019-05-12 03:08:28 +0500

    recipe list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes:Recipe[]=[
+  new Recipe('Chicken Kentukki','thi is a burger recipe','https://www.kfc.com.au/sites/default/files/alohadata/images/products/Kentucky_Burger_web_hero.jpg'),
+  new Recipe('Zinger','this is a zinger burger','https://www.kfc.com.au/sites/default/files/alohadata/images/products/Kentucky_Burger_web_hero.jpg')
+];
   constructor() { }
 
   ngOnInit() {
