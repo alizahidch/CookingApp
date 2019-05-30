8681dfc5e7c1fb818afa3c42d7236effc1255e36
alizahidch <cmalizahid@gmail.com>
2019-05-30 23:53:05 +0500

    shopping-edit

--- src/app/shopping/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, ElementRef, ViewChild,EventEmitter } from '@angular/core';
+import {Ingridient} from '../../../shared/ingridient.model'
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+@ViewChild('nameInput') nameInputRef:ElementRef;
+@ViewChild('amountInput') amountInputRef:ElementRef;
+@Output() ingridientAdded=new EventEmitter<Ingridient>()
   constructor() { }
 
   ngOnInit() {
+  }
+  check(){
+    console.log("Hello")
+  }
+
+  onAddItem(){
+    const ingName=this.nameInputRef.nativeElement.value;
+    const ingAmount=this.amountInputRef.nativeElement.value;
+const newIngridient=new Ingridient(ingName,ingAmount);
+this.ingridientAdded.emit(newIngridient); 
   }
 
 }
